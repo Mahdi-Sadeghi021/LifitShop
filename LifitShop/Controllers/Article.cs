d5664657281d721657f1c5908efcd0a76ab90fc6
mahdi_021 <sadghimahdi09@gmail.com>
2025-08-27 04:25:53 -0700

    add 6 veblag details

---
+++ LifitShop/Controllers/Article.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LifitShop.Controllers
+{
+    public class Article : Controller
+    {
+        public IActionResult Ganoderma()
+        {
+            return View();
+        }
+        public IActionResult HairSorom()
+        {
+            return View();
+        }
+        public IActionResult Protoin()
+        {
+            return View();
+        }
+        public IActionResult kaviar()
+        {
+            return View();
+        }
+        public IActionResult pankeick()
+        {
+            return View();
+        }
+        public IActionResult facewash()
+        {
+            return View();
+        }
+    }
+}
