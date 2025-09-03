a1560a01fbd8ba06b32de10f01b33925f76ebc17
Shahab <shahabshahrami27@gmail.com>
2025-09-04 01:26:03 +0330

    a half of register page

---
+++ LifitShop/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+
+namespace LifitShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.PhoneNumber.ToString()};
+                var res = await _userManager.CreateAsync(user);
+                if (res.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+    }
+}
--- LifitShop/Program.cs
+++ LifitShop/Program.cs
@@ -70,7 +70,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
